d7a4aa3cecc1834466b4cd1ab4884a4e1a4396c9
Dema Maksim <maksimdema@gmail.com>
2021-01-22 22:08:48 +0200

    добавляет решение

---
+++ src/js/timer.js
@@ -0,0 +1,37 @@
+const refs = {
+  //   daysValue: document.querySelector('[data-value="days"]'),
+  //   hoursValue: document.querySelector('[data-value="hours"]'),
+  //   minsValue: document.querySelector('[data-value="mins"]'),
+  //   secsValue: document.querySelector('[data-value="secs"]'),
+
+  timer: document.querySelector('#timer-1'),
+};
+
+const timer = {
+  start() {
+    const targetDate = new Date('Jul 21, 2021');
+
+    setInterval(() => {
+      const currentDate = Date.now();
+      let deltaTime = targetDate - currentDate;
+      updateClockface(deltaTime);
+    }, 1000);
+  },
+};
+
+timer.start();
+
+function updateClockface(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  refs.timer.textContent = `${days}:${hours}:${mins}:${secs}`;
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
