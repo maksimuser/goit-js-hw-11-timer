637715614a9edd87e9a97a79a75c3234d19a135a
Dema Maksim <maksimdema@gmail.com>
2021-01-25 20:51:09 +0200

    добавляет обновление timer

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,32 +1,43 @@
-const refs = {
-  timer: document.querySelector('#timer-1'),
-};
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
 
-const timer = {
   start() {
-    const targetDate = new Date('Jul 21, 2021');
-
+    this.updateClockface(0);
     setInterval(() => {
-      const currentDate = Date.now();
-      let deltaTime = targetDate - currentDate;
-      updateClockface(deltaTime);
+      const currentTime = Date.now();
+
+      let deltaTime = this.targetDate - currentTime;
+      this.updateClockface(deltaTime);
     }, 1000);
-  },
-};
+  }
+
+  updateClockface(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    document.querySelector(
+      this.selector,
+    ).textContent = `${days}:${hours}:${mins}:${secs}`;
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('dec 31, 2021'),
+});
 
 timer.start();
-
-function updateClockface(time) {
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-  );
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-  refs.timer.textContent = `${days}:${hours}:${mins}:${secs}`;
-}
-
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
