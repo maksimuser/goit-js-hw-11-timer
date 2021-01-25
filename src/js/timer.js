229f750f9df2cda5168565ffc73ab2ecc6db80b8
Dema Maksim <maksimdema@gmail.com>
2021-01-25 10:52:16 +0200

    Update timer.js

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,9 +1,4 @@
 const refs = {
-  //   daysValue: document.querySelector('[data-value="days"]'),
-  //   hoursValue: document.querySelector('[data-value="hours"]'),
-  //   minsValue: document.querySelector('[data-value="mins"]'),
-  //   secsValue: document.querySelector('[data-value="secs"]'),
-
   timer: document.querySelector('#timer-1'),
 };
 
